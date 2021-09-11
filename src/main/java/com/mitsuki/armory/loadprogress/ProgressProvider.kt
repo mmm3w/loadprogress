358c3b031fbcdaadac0e5787f2109f4314e48a65
Mitsuki <psnhewzc@gmail.com>
2021-09-12 00:27:05 +0800

    fix)class package error

--- src/main/java/com/mitsuki/armory/loadprogress/ProgressProvider.kt
+++ src/main/java/com/mitsuki/armory/loadprogress/ProgressProvider.kt
@@ -1,4 +1,4 @@
-package com.mitsuki.loadprogress
+package com.mitsuki.armory.loadprogress
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
